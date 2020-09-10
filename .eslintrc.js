83e291d0672a16accad85879072295d5af1d53d9
Francis Cantarin <franncisjc@gmail.com>
2020-09-10 16:24:04 -0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
---
+++ README.md
@@ -0,0 +1,2 @@
+# template_node_js_express
+ base template of a node application with js and express
